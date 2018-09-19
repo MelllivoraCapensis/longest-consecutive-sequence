f12ba2a4b799956ea39d0ee6abfc1b1741b4041e
MelllivoraCapensis <__>
2018-09-19 13:39:45 +0300

    21 failed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 module.exports=function longestConsecutiveLength(array) {
+	function has_elem(array,elem)
+   {
+   	for(var i=0;i<array.length;i++) 
+   	    if(array[i]===elem)
+   	    {
+   	    	return i;
+   	    }
+   	return -1;
+   }
    var result=0;
+   var lcl=0;
    while(array.length>0) {
     var neighb_left=array[0]-1;
     var neighb_right=array[0]+1;
@@ -7,26 +17,21 @@ module.exports=function longestConsecutiveLength(array) {
    	lcl_arr.push(array[0]);
    	array.splice(0,1);
    	var lcl=1;
-   	var left_pos=array.indexOf(neighb_left,0);
-   	var right_pos=array.indexOf(neighb_right,0);
-   	while(left_pos>=0||right_pos>=0)
+   	var left_pos=has_elem(array,neighb_left);
+   	while(left_pos>=0)
    	{
-   		 if(left_pos>=0)
-         {
-         	lcl++;
-         	lcl_arr.push(array[left_pos]);
-         	array.splice(left_pos,1);
-         	neighb_left--;
-         }   	
-         if(right_pos>=0)
-         {
-         	lcl++;
-         	lcl_arr.push(array[right_pos]);
-         	array.splice(right_pos,1);
-         	neighb_right++;
-         } 
-    left_pos=array.indexOf(neighb_left,0);
-   	right_pos=array.indexOf(neighb_right,0);
+   		lcl++;
+        array.splice(left_pos,1);
+        neighb_left--;
+        left_pos=has_elem(array,neighb_left);
+    }
+    var right_pos=has_elem(array,neighb_right);
+    while(right_pos>=0)
+   	{
+       	lcl++;
+       	array.splice(right_pos,1);
+       	neighb_right++;
+     	right_pos=has_elem(array,neighb_right);
     }
    result=Math.max(result,lcl);
      }
@@ -34,4 +39,4 @@ return result;
 }
 
 
-
+ 
