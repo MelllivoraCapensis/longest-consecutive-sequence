4e5291e906cb89edc4aca6b6bcd20de9933c453b
MelllivoraCapensis <kobzarevdmitry@yandex.ru>
2018-09-20 14:09:34 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 module.exports=function longestConsecutiveLength(array) {
-  function has_elem(array,elem)
-   {
-    for(var i=0;i<array.length;i++) 
-        if(array[i]===elem)
-        {
-          return i;
-        }
-    return -1;
-   }
    var result=0;
-   var lcl=0;
    while(array.length>0) {
     var neighb_left=array[0]-1;
     var neighb_right=array[0]+1;
@@ -20,8 +10,6 @@ module.exports=function longestConsecutiveLength(array) {
     var left_pos=array.indexOf(neighb_left,0);
     var right_pos=array.indexOf(neighb_right,0);
     while(left_pos>=0||right_pos>=0)
-    var left_pos=has_elem(array,neighb_left);
-    while(left_pos>=0)
     {
        if(left_pos>=0)
          {
@@ -39,20 +27,9 @@ module.exports=function longestConsecutiveLength(array) {
          } 
     left_pos=array.indexOf(neighb_left,0);
     right_pos=array.indexOf(neighb_right,0);
-      lcl++;
-        array.splice(left_pos,1);
-        neighb_left--;
-        left_pos=has_elem(array,neighb_left);
-    }
-    var right_pos=has_elem(array,neighb_right);
-    while(right_pos>=0)
-    {
-        lcl++;
-        array.splice(right_pos,1);
-        neighb_right++;
-      right_pos=has_elem(array,neighb_right);
     }
    result=Math.max(result,lcl);
      }
 return result;
 }
+
